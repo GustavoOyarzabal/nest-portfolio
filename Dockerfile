6fc747a569455a1718c9e08e4663d4f502a5279d
Gustavo Oyarzabal <oyarzabal.gustavo@gmail.com>
2024-06-03 13:45:10 +0200

    docker

--- Dockerfile
+++ Dockerfile
@@ -2,24 +2,36 @@ FROM node:18
 
 WORKDIR /usr/src/app
 
-RUN npm install -g pnpm
+# Instalation global de pnpm and Cli
+RUN npm install -g pnpm @nestjs/cli
 
-COPY pnpm-lock.yaml ./
-COPY package.json ./
+# 
+COPY pnpm-lock.yaml package.json ./
 
+# Copy package.json => adresse work
+#COPY pnpm-lock.yaml ./
+#COPY package.json ./
+
+# dependencies
 RUN pnpm install
-
-RUN pnpm add -g @nestjs/cli
 
 COPY . .
 
-RUN chmod -R +x node_modules/.bin
+# scripts => node_modules/.bin =>licencie de execution
+RUN chmod -R +x /usr/src/app/node_modules/.bin
+# lic de nest
+RUN [ -e /usr/local/bin/nest ] || ln -s /usr/src/app/node_modules/@nestjs/cli/bin/nest.js /usr/local/bin/nest
 
+# build 
 RUN pnpm run build
 
 EXPOSE 3001
 
 CMD ["pnpm", "run", "start:prod"]
+
+
+
+
 
 
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,28 @@ import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { EnvConfiguration } from './config/env.config';
 import { JoiValidationSchema } from './config/joi.validation';
 import { PortfoliosModel } from './portfolios/portfolios.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [EnvConfiguration], //conversiones y mapeos
+      load: [EnvConfiguration], // conversiones y mapeos
       validationSchema: JoiValidationSchema,
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/portfolios_portfolio'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('mongodb'),
+      }),
+      inject: [ConfigService],
+    }),
     PortfoliosModel,
-
     CommonModule,
   ],
 })
@@ -27,3 +33,33 @@ export class AppModule {
   //   // console.log(process.env);
   // }
 }
+
+// import { Module } from '@nestjs/common';
+// import { join } from 'path';
+// import { ServeStaticModule } from '@nestjs/serve-static';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { CommonModule } from './common/common.module';
+// import { ConfigModule } from '@nestjs/config';
+// import { EnvConfiguration } from './config/env.config';
+// import { JoiValidationSchema } from './config/joi.validation';
+// import { PortfoliosModel } from './portfolios/portfolios.module';
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({
+//       load: [EnvConfiguration], //conversiones y mapeos
+//       validationSchema: JoiValidationSchema,
+//     }),
+//     ServeStaticModule.forRoot({
+//       rootPath: join(__dirname, '..', 'public'),
+//     }),
+//     MongooseModule.forRoot('mongodb://localhost:27017/portfolios_portfolio'),
+//     PortfoliosModel,
+
+//     CommonModule,
+//   ],
+// })
+// export class AppModule {
+//   // constructor() {
+//   //   // console.log(process.env);
+//   // }
+// }
